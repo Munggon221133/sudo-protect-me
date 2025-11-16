408b22c581302268cc237a1619adc08cebf9cde8
nuttee02092545 <nuttee02092545@gmail.com>
2025-11-16 09:19:13 +0000

    feat: Update new api url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://sudo-protect-me-backend-n01gisuif-munggon221133s-projects.vercel.app',
+        target: 'https://sudo-protect-me-backend-git-main-munggon221133s-projects.vercel.app?_vercel_share=5QKfO4hFuk5JT9MSVtfWSkjCyChRzChc',
         changeOrigin: true,
       },
     },
